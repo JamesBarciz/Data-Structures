958e12de7b52ad2372bf5153be8781d6dba2a45e
James Barciz <jamesjbarciz@gmail.com>
2020-09-15 22:39:18 -0400

    begin assignment

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value, next_node):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+
+class LinkedList:
+    pass
